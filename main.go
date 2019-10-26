7e19a9428b0ededc2f15610ba3b276b463a5cd22
Caleb Schoepp <caleb.schoepp@gmail.com>
2019-10-25 21:59:13 -0600

    Server can successfully receive POST to /api and use data

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/api/", handleAPI)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, err.Error(), 500)
+	}
+	tmpl.Execute(w, nil)
+}
+
+func handleAPI(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		log.Println("Endpoint only accepts POSTs")
+		http.Error(w, "Endpoint only accepts POSTs", 500)
+		return
+	}
+	_50k := int64((1 << 10) * 50)
+	err := r.ParseMultipartForm(_50k)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	log.Println(r.MultipartForm.Value["From"][0])
+
 }
