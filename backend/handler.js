c6132ba55c14ca5593080b05f490a35ec9b12334
Caleb Schoepp <caleb.schoepp@gmail.com>
2019-11-20 22:29:34 -0700

    Decent state, before testing out multi-part form parsing

--- backend/handler.js
+++ backend/handler.js
@@ -1,18 +1,27 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async event => {
+module.exports.parseEmail = async event => {
+  console.log(event);
+  let data = {};
+  try {
+    data = JSON.parse(event.body);
+  } catch (jsonError) {
+    console.log("There was an error parsing the body", jsonError);
+    return {
+      statusCode: 400
+    };
+  }
+
+  if (typeof data["stripped-html"] === "undefined") {
+    console.log("stripped-html was not parameter");
+    return {
+      statusCode: 400
+    };
+  }
+
+  console.log(data["stripped-html"]);
+
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    statusCode: 201
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
