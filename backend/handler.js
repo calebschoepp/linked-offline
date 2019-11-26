2ba0448762d7b825feb615fa96f54b12dbf1b90b
Caleb Schoepp <caleb.schoepp@gmail.com>
2019-11-25 23:15:33 -0700

    More work on the failed lambda handling of multipart POST

--- backend/handler.js
+++ backend/handler.js
@@ -1,28 +1,36 @@
 "use strict";
 
 module.exports.parseEmail = async event => {
-  queryString = require("querystring");
-  console.log(event);
-  console.log(event.headers);
-  console.log(event.body);
+  // queryString = require("querystring");
   console.log(typeof event.body);
   console.log(typeof event.headers);
-  let body = new String(queryString.unescape(event.body));
+  let body = new String(decodeURIComponent(event.body));
+  console.log(typeof body);
   console.log("Body");
   console.log(body);
 
+  let strip = body.match(/(stripped-html)/).index - 1;
+  console.log("Strip");
+  console.log(strip);
+  let start = 0;
+  try {
+    start = body.slice(strip).match(/(<html>)/).index;
+  } catch {
+    console.log("Failed start regex");
+  }
+  console.log("start");
+  console.log(start);
+  let end = body.length;
+  try {
+    end = body.slice(strip).match(/(<\/html>)/).index + 7;
+  } catch {
+    console.log("failed end regex");
+  }
+  console.log("end");
+  console.log(end);
+  console.log(body.slice(start + strip, end + strip));
   return {
     statusCode: 200,
     body: "Hello"
   };
-  let strip = body.match(/(stripped-html)/).index - 1;
-  console.log("Strip");
-  console.log(strip);
-  let start = body.slice(strip).match(/(<html>)/).index;
-  console.log("start");
-  console.log(start);
-  let end = body.slice(strip).match(/(<\/html>)/).index + 7;
-  console.log("end");
-  console.log(end);
-  console.log(body.slice(start + strip, end + strip));
 };
