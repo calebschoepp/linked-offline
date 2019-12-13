a693807a55cbddab714bbce56d5d921f56cda5f0
Caleb Schoepp <caleb.schoepp@gmail.com>
2019-12-13 01:06:11 -0700

    PyMuPDF successfully added as a dependency

--- serverless-backend/handler2.py
+++ serverless-backend/handler2.py
@@ -1,7 +1,24 @@
+import PyPDF4
+import fitz
+import boto3
+
 def test(event, context):
-    print(event)
-    response = {
-        "statusCode": 200,
-        "body": "it worked"
-    }
+    s3 = boto3.resource('s3')
+    pdf = s3.Object('url-pdfs','01bb31b0cb2639e7fe73c1ee5b2a7696.pdf').get()
+    pdf_content = pdf['Body'].read()
+
+    doc = fitz.open("pdf", pdf_content)
+    page = doc[0]
+    coords = page.searchFor('dev')
+
+    if coords:
+        response = {
+            "statusCode": 200,
+            "body": "good"
+        }
+    else:
+        response = {
+            "statusCode": 200,
+            "body": "good"
+        }
     return response
